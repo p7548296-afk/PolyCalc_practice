b137ea52d4dfdf66a3f48c866869d5ada7c51f4d
minseo <p7548296@gmail.com>
2026-04-20 21:07:08 +0900

    start

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -2,27 +2,12 @@ package com.ll;
 
 public class Calc {
     public static int run(String expression) {
-        if (expression.contains("+")) {
-            String[] bits = expression.split("\\s*\\+\\s*");
-            return sum(bits, 0);
-        }
+        String[] expressionBits = expression.split(" \\+ ");
 
-        String[] bits = expression.split("\\s*-\\s*");
-        int num1 = Integer.parseInt(bits[0].trim());
-        int num2 = Integer.parseInt(bits[1].trim());
+        int num1 = Integer.parseInt(expressionBits[0]);
+        int num2 = Integer.parseInt(expressionBits[1]);
 
-        return subtract(num1, num2);
-    }
-
-    private static int sum(String[] bits, int idx) {
-        if (idx == bits.length) return 0;
-        return Integer.parseInt(bits[idx].trim()) + sum(bits, idx + 1);
-    }
-
-    private static int subtract(int num1, int num2) {
-        if (num2 == 0) return num1;
-        if (num2 > 0) return subtract(num1 - 1, num2 - 1);
-        return subtract(num1 + 1, num2 + 1);
+        return num1 + num2;
     }
 
 }
